8082972298ae825d5a3cde2e48f0a0775aa7f4fb
Yodelyoda <47818799+Yodelyoda@users.noreply.github.com>
2021-05-27 10:42:40 +0300

    Add files via upload

---
+++ lab12.6.cpp
@@ -0,0 +1,72 @@
+#define QMAX 100
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+typedef int Info;
+struct queue {
+	int qu[QMAX];
+	int rear, frnt;
+};
+void init(struct queue* q) {
+	q->frnt = 1;
+	q->rear = 0;
+	return;
+}
+void insert(struct queue* q, int x) {
+	if (q->rear < QMAX - 1) {
+		q->rear++;
+		q->qu[q->rear] = x;
+	}
+	else
+		printf("Черга повна\n");
+	return;
+}
+int isempty(struct queue* q) {
+	if (q->rear < q->frnt)    return 1;
+	else  return 0;
+}
+void print(struct queue* q) {
+	int h;
+	if (isempty(q) == 1) {
+		printf("Черга пуста\n");
+		return;
+	}
+	for (h = q->frnt; h <= q->rear; h++)
+		printf("%d ", q->qu[h]);
+	return;
+}
+int remove(struct queue* q) {
+	int x;
+	if (isempty(q) == 1) {
+		printf("Черга пуста\n");
+		return(0);
+	}
+	x = q->qu[q->frnt];
+	q->frnt++;
+	return x;
+}
+int main()
+{
+	struct queue* q;
+	int a;
+	system("chcp 1251");
+	system("cls");
+	q = (queue*)malloc(sizeof(queue));
+	init(q);
+	print(q);
+	for (int i = 0; i < 8; i++) {
+		printf("Введіть елемент черги: ");
+		scanf("%d", &a);
+		insert(q, a);
+	}
+	printf("\n");
+	print(q);
+	while (q->frnt <= q->rear) {
+		a = remove(q);
+		printf("\nВидален елемент %d\n", a);
+		print(q);
+	}
+	getchar();
+	return 0;
+}
